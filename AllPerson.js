59b46817ac731543d99f5bc8aa755359f80c4009
Talita Beneves <talitabeneves15@gmail.com>
2021-12-22 00:18:52 -0300

    location.assign

---
+++ AllPerson.js
@@ -0,0 +1,11 @@
+function Chaves(){
+  location.assign("./Chaves/ChavesFase1.html");
+}
+
+function Chiquinha(){
+  location.assign("./Chiquinha/ChiquinhaFase1.html");
+}
+
+function Madruga(){
+  location.assign("./Sr.Madruga/MadrugaFase1.html");
+}
